773ba9761760f297d9f0b47ec2da918c081fa9ff
WeatherQuake <178542190+weatherequake@users.noreply.github.com>
2024-09-23 20:28:14 +0900

    constant.members.js の更新

--- js/constant.members.js
+++ js/constant.members.js
@@ -108,17 +108,37 @@ const members = {
         {
             name : "Shirahama_EEW",
             description : "Shirahama_EEWです。小学生の頃から地震や津波などに興味を持ち、現在は地震観測をしています。 少しでも皆さんのお力になれるような情報発信を可能な限りやって行きたいと思います。",
-            icon : "shimahara.webp"
+            icon : "shirahama.webp"
         },
         {
             name : "かたこふー",
             description : "かたこふーです！地震災害に興味のある学生です。YouTubeを通じたライブ配信や、X（旧Twitter）での防災の呼びかけなどを行なっています。",
             icon : "katakofu.webp"
+            sns : [
+                {
+                    url : "https://twitter.com/katakohu",
+                    name : "X"
+                },
+                {
+                    url : "https://www.youtube.com/@katakohu_eq",
+                    name : "Youtube"
+                },
+            ]
         },
         {
             name : "ランサブ",
             description : "地震のシミュレーション動画を作るのが趣味です。地震のライブ配信とかもしています。",
             icon : "ransub.webp"
+            sns : [
+                {
+                    url : "https://twitter.com/ransabu_5656",
+                    name : "X"
+                },
+                {
+                    url : "https://www.youtube.com/@ランサブ",
+                    name : "Youtube"
+                },
+            ]
         },
         {
             name : "砂漠の砂",
@@ -147,6 +167,12 @@ const members = {
             name : "Mr.ドラ",
             description : "Mr.ドラです。気象・地震に興味があり、気象や地震のWebアプリを作っています。誰もが使いやすいアプリを作れるように頑張ります。",
             icon : "mrdora.jpg"
+            sns : [
+                {
+                    url : "https://twitter.com/Mr82697235",
+                    name : "X"
+                },
+            ]
         },
         {
             name : "WeatherQuake",
